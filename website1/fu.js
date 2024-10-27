d99ac22e106a6b50ed78e52527d9ba1c84c33dd8
okyang <oknyhm@outlook.com>
2024-10-27 16:19:02 +0800

    g

--- website1/fu.js
+++ website1/fu.js
@@ -6,3 +6,13 @@ function toggleMenu() {
       menuContent.style.display = "block";
     }
   }
+//分割线-------------------------------------------------
+
+  document.querySelector('.toggle-button').addEventListener('click', () => {
+    const menu2 = document.querySelector('.menu2');
+    if (menu2.style.display === 'flex') {
+        menu2.style.display = 'none';
+    } else {
+        menu2.style.display = 'flex';
+    }
+});
