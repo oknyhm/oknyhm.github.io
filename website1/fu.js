c2688d775894df80add9ff62ca756cbfcb91eac6
okyang <oknyhm@outlook.com>
2024-10-31 00:11:52 +0800

    1

--- website1/fu.js
+++ website1/fu.js
@@ -16,3 +16,17 @@ document.querySelector('.toggle-button').addEventListener('click', () => {
       menu2.style.display = 'flex';
   }
 });
+
+
+function closePopup() {
+  document.getElementById('popup').style.display = 'none';
+}
+
+function submitInput() {
+  let input = document.getElementById('userInput').value;
+  alert("您输入的信息是: " + input);
+  closePopup();
+  const containerToDelete = document.getElementById('containerToDelete');//搞到容器信息
+  containerToDelete.remove();//拆了!
+}
+
